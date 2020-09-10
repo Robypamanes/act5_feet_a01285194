1e2e729d96abfaf07b28693d654d3c938c0d9a90
Robypamanes <70342514+Robypamanes@users.noreply.github.com>
2020-09-10 13:53:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    yards = feet / 3
+    basic.showNumber(yards)
+    basic.showString("yards")
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+let yards = 0
+let feet = 0
+feet = randint(1, 10)
